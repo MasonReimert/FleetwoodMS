a4d7c42af4d385e713769465880a7f10e528390b
Mason Reimert <mason@laughablegames.com>
2016-03-27 12:05:16 -0400

    COMMIT TO ADD AD CODE

---
+++ Amazon Devices/FleetwoodMS/app/src/main/java/com/laughablegames/fleetwoodms/URLS.java
@@ -0,0 +1,14 @@
+package com.laughablegames.fleetwoodms;
+
+/**
+ * Created by Mason on 2/28/16.
+ */
+public class URLS {
+        private String URL;
+
+        public String getURL() {
+            return URL;
+        }
+
+
+}
